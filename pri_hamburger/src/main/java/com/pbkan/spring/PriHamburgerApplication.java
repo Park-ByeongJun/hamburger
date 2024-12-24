d3ddac7db88daab08e2c19c679b9eb031c61eb40
pbkan <pbkan4@gmail.com>
2024-12-24 16:43:18 +0900

    [박병준]개인 프로젝트

---
+++ pri_hamburger/src/main/java/com/pbkan/spring/PriHamburgerApplication.java
@@ -0,0 +1,13 @@
+package com.pbkan.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriHamburgerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PriHamburgerApplication.class, args);
+	}
+
+}
---
+++ pri_hamburger/src/test/java/com/pbkan/spring/PriHamburgerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pbkan.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriHamburgerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
